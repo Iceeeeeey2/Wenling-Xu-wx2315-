aa9654b75ca8562709ee3e0d07d6cfe6e715dec9
(wx2315) <166080406+Iceeeeeey2@users.noreply.github.com>
2024-04-11 19:26:29 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,9 +1,9 @@
-def getBondPrice_E(face, couponRate, yc):
+def getBondPrice_E(face, couponRate, m, yc):
     pvcfsum = 0
     cf = couponRate * face
     for t, yc_rate in enumerate(yc, start=1):  
         pv = (1 + yc_rate) ** (-t)  
         pvcf = pv * cf
         pvcfsum += pvcf
-    bondprice = pvcfsum + (1 + yc[-1]) ** (-len(yc)) * face 
+    bondprice = pvcfsum + (1 + yc[-1]) ** (-m) * face 
     return bondprice
